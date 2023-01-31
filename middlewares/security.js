3775ecd25f73034e4fd6de404deac8d04810324a
Alan Gimenez <gimenezalan00@gmail.com>
2023-01-31 18:45:20 -0300

    delete logs from security middleware and add readme for deploys

--- middlewares/security.js
+++ middlewares/security.js
@@ -3,9 +3,6 @@ class MidSecurity {
 
     checkPassword (req, res, next) {
         // const indexHeader = req.headers.findIndex((e) => e == 'Password-Security')
-        console.log(req.headers)
-        console.log(process.env.WORD_SECRET)
-        console.log(req.headers.password)
         if (req.headers.password == process.env.WORD_SECRET) {
             next()
         } else {
