ec3dfb8e5d07dae64f52ad12248dce948f95daa3
AlanGimenez <gimenezalan00@gmail.com>
2023-06-06 20:52:09 -0300

    add fetch to coingecko api to get cripto quotes

--- services/investments/otherQuotesService.js
+++ services/investments/otherQuotesService.js
@@ -6,26 +6,43 @@ moment().format();
 const fetch = require('node-fetch')
 
 class OtherQuotesService {
-    constructor(){}
+    constructor() { }
 
-    async uploadNewQuote (request) {
+    async uploadNewQuote(request) {
         const quote = convertRequest(request)
+
+        let sleep = function (ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        };
 
         const response = await fetch('https://criptoya.com/api/dolar')
         const data = await response.json()
+
+        const ethereumResponse = await fetch('https://api.coingecko.com/api/v3/coins/ethereum')
+        const ethereumData = await ethereumResponse.json()
+
+        const litecoinResponse = await fetch('https://api.coingecko.com/api/v3/coins/litecoin')
+        const litecoinData = await litecoinResponse.json()
+
+        const bitcoinResponse = await fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
+        const bitcoinData = await bitcoinResponse.json()
+
         await otherQuotesDao.subirInfo({
             date: quote.date,
             quotes: {
                 dolarbnacomprador: data.oficial,
                 dolarbnavendedor: data.oficial - 12,
-                dolarmep: data.mep
+                dolarmep: data.mep,
+                ethereum: ethereumData.market_data.current_price.usd,
+                bitcoint: bitcoinData.market_data.current_price.usd,
+                litecoin: litecoinData.market_data.current_price.usd
             }
         })
-        
-        return ({'message': 'ok'})
+
+        return ({ 'message': 'ok' })
     }
 
-    async getLastQuote () {
+    async getLastQuote() {
         const lastQuote = await otherQuotesDao.getLastQuote()
         // const date = new Date(lastQuote[0].date)
         return ({
