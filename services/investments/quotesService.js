457e188841f308a79e75d8564d3ae5306147673a
AlanGimenez <gimenezalan00@gmail.com>
2023-06-16 11:52:42 -0300

    add endpoint to save info from IOL

--- services/investments/quotesService.js
+++ services/investments/quotesService.js
@@ -1,6 +1,8 @@
 const quotesRepository = require('../../repository/daos/investments/quotesDao')
 const lastValueService = require('./lastValueService.js')
+const userService = require('../user/userService')
 const Quote = require('../../models/quote');
+const fetch = require('node-fetch')
 
 class QuotesService {
     constructor() { }
@@ -49,6 +51,50 @@ class QuotesService {
 
         // response
         return {"message": "ok"}
+    }
+
+    async saveInfoFromIol() {
+        const token = await userService.getAccessTokenToOperateIol()
+
+        const onQuotes = await this.#getOnQuotes(token)
+        const adrQuotes = await this.#getAdrQuotes(token)
+        const publicBondsQuotes = await this.#getPublicBondsQuotes(token)
+
+        await quotesRepository.subirInfo({
+            date: new Date(),
+            quotes: {
+                obligacionesNegociables: onQuotes,
+                adr: adrQuotes,
+                publicBonds: publicBondsQuotes
+            }
+        })
+    }
+
+    async #getOnQuotes(token) {
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` }
+        }
+        const quotesResponse = await fetch(`https://api.invertironline.com/api/v2/Cotizaciones/obligacionesNegociables/argentina/Todos`, requestOptions)
+        return await quotesResponse.json() 
+    }
+
+    async #getAdrQuotes(token) {
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` }
+        }
+        const quotesResponse = await fetch(`https://api.invertironline.com/api/v2/Cotizaciones/aDRs/estados_Unidos/Todos`, requestOptions)
+        return await quotesResponse.json() 
+    }
+
+    async #getPublicBondsQuotes(token) {
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` }
+        }
+        const quotesResponse = await fetch(`https://api.invertironline.com/api/v2/Cotizaciones/bonos/bCBA/argentina`, requestOptions)
+        return await quotesResponse.json() 
     }
 }
 
