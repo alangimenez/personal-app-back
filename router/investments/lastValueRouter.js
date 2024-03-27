f436327e81273569747160a71854409bbe877841
AlanGimenez <gimenezalan00@gmail.com>
2024-03-27 11:38:02 -0300

    fix last value endpoint to get quotes

--- router/investments/lastValueRouter.js
+++ router/investments/lastValueRouter.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const lastValueService = require('../../services/investments/lastValueService');
 
-router.get('/', async (req, res) => {
-    const datos = await lastValueService.getAll()
-    res.status(200).json(datos)
-})
-
 router.get('/tir', async (req, res) => {
     const result = await lastValueService.getQuotesWithTir();
     res.json(result)
